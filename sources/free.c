5639ff2d36aee5826dfe20e57b4f8b8a1eb93770
CanIBeFii <filipe250502@gmail.com>
2023-02-10 12:33:07 +0000

    Free mutexs correctly

--- sources/free.c
+++ sources/free.c
@@ -6,22 +6,36 @@
 /*   By: fialexan <fialexan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 12:35:44 by fialexan          #+#    #+#             */
-/*   Updated: 2023/02/06 13:56:11 by fialexan         ###   ########.fr       */
+/*   Updated: 2023/02/10 12:27:54 by fialexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-int	free_philosopher(t_philo *philo, pthread_mutex_t *fork, int num)
+int	free_philosopher(t_table *table, int num)
 {
 	int	iter;
 
-	iter = 0;
-	while (iter < num)
+	if (table != NULL)
 	{
-		free(&philo[iter]);
-		free(&fork[iter]);
-		iter++;
+		if (table->forks != NULL)
+		{
+			iter = 0;
+			while (iter < num)
+			{
+				pthread_mutex_destroy(table->forks + iter);
+				iter++;
+			}
+		}
+		if (table->philo != NULL)
+		{
+			iter = 0;
+			while (iter < num)
+			{
+				free(table->philo + iter);
+				iter++;
+			}
+		}
 	}
 	return (1);
 }
