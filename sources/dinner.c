23a8fc35cc2fae5e27bbf4596c75bf456911d5ef
CanIBeFii <filipe250502@gmail.com>
2023-02-10 12:31:03 +0000

    Change the argument received by dinner

--- sources/dinner.c
+++ sources/dinner.c
@@ -6,34 +6,34 @@
 /*   By: fialexan <fialexan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 13:59:37 by fialexan          #+#    #+#             */
-/*   Updated: 2023/02/08 15:01:48 by fialexan         ###   ########.fr       */
+/*   Updated: 2023/02/10 11:48:21 by fialexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-int	dinner(t_philo *philo, pthread_mutex_t *forks, int philo_num);
+// int	dinner(t_philo *philo, pthread_mutex_t *forks, int philo_num);
 
-int	dinner(void *args)
+void	*dinner(void *args)
 {
-	t_table		table;
+	t_philo		*philo;
 	long long	time;
 
-	table = *args;
+	philo = args;
 	time = get_time_of_day();
-	print_message(DIED_CODE, time - philo.last_time_ate, philo.philo_num);
-	philo.last_time_ate = time;
+	print_message(DIED_CODE, time - philo->last_time_ate, philo->philo_num);
+	philo->last_time_ate = time;
 	time = get_time_of_day();
-	print_message(FORK_CODE, time - philo.last_time_ate, philo.philo_num);
-	philo.last_time_ate = time;
+	print_message(FORK_CODE, time - philo->last_time_ate, philo->philo_num);
+	philo->last_time_ate = time;
 	time = get_time_of_day();
-	print_message(EAT_CODE, time - philo.last_time_ate, philo.philo_num);
-	philo.last_time_ate = time;
+	print_message(EAT_CODE, time - philo->last_time_ate, philo->philo_num);
+	philo->last_time_ate = time;
 	time = get_time_of_day();
-	print_message(SLEEP_CODE, time - philo.last_time_ate, philo.philo_num);
-	philo.last_time_ate = time;
+	print_message(SLEEP_CODE, time - philo->last_time_ate, philo->philo_num);
+	philo->last_time_ate = time;
 	time = get_time_of_day();
-	print_message(THINK_CODE, time - philo.last_time_ate, philo.philo_num);
-	philo.last_time_ate = time;
-	return (0);
+	print_message(THINK_CODE, time - philo->last_time_ate, philo->philo_num);
+	philo->last_time_ate = time;
+	return (NULL);
 }
