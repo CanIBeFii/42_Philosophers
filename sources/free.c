b23c370b80b347f702128990da176bba47493f7f
CanIBeFii <filipe250502@gmail.com>
2023-02-10 16:06:02 +0000

    Solver error when freeing more than once the philo

--- sources/free.c
+++ sources/free.c
@@ -6,7 +6,7 @@
 /*   By: fialexan <fialexan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 12:35:44 by fialexan          #+#    #+#             */
-/*   Updated: 2023/02/10 12:27:54 by fialexan         ###   ########.fr       */
+/*   Updated: 2023/02/10 16:03:43 by fialexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int	free_philosopher(t_table *table, int num)
 {
 	int	iter;
 
+	usleep(1000);
 	if (table != NULL)
 	{
 		if (table->forks != NULL)
@@ -26,16 +27,10 @@ int	free_philosopher(t_table *table, int num)
 				pthread_mutex_destroy(table->forks + iter);
 				iter++;
 			}
+			free(table->forks);
 		}
 		if (table->philo != NULL)
-		{
-			iter = 0;
-			while (iter < num)
-			{
-				free(table->philo + iter);
-				iter++;
-			}
-		}
+			free(table->philo);
 	}
 	return (1);
 }
