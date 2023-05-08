2505302887f258e8189705a081918183e89d0dbe
CanIBeFii <filipe250502@gmail.com>
2023-05-08 13:05:31 +0100

    fix: update lib with new funcitons

--- includes/philosophers.h
+++ includes/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: fialexan <fialexan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 17:33:37 by fialexan          #+#    #+#             */
-/*   Updated: 2023/05/05 14:23:58 by fialexan         ###   ########.fr       */
+/*   Updated: 2023/05/08 12:16:10 by fialexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@
 // DEFINES
 
 typedef pthread_mutex_t	t_mutex;
+typedef pthread_t		t_thread;
 
 typedef struct s_info
 {
@@ -39,6 +40,7 @@ typedef struct s_info
 typedef struct s_philo
 {
 	int			id;
+	t_thread	thread;
 	t_mutex		*left_fork;
 	t_mutex		*right_fork;
 	int			num_of_meals;
@@ -54,30 +56,38 @@ typedef struct s_philo
 # define FORKS_INITIALIZATION 30
 # define PHILOS_INITIALIZATION 40
 
+// PHILOSOPHERS.C
+
+void		start_dinner(t_info *info, t_philo *philos);
+
 // PARSER.C
 
-t_info	*parse_input(int argc, char **argv);
+t_info		*parse_input(int argc, char **argv);
 
 // INITIALIZATION.C
 
-t_info	*init_info(int argc, char **argv);
+t_info		*init_info(int argc, char **argv);
 
-t_info	*init_forks(t_info *info);
+t_info		*init_forks(t_info *info);
 
-t_philo	*init_philos(t_info *info);
+t_philo		*init_philos(t_info *info);
 
 // ERROR_HANDLING.C
 
-int		handle_error(int error_code, int return_value);
+int			handle_error(int error_code, int return_value);
 
 // FREE.C
 
-void	free_info(t_info *info);
+void		free_info(t_info *info);
 
 // UTIL_FUNCTIONS.C
 
-int		ft_atoi(char *str);
+int			ft_atoi(char *str);
 
-int		check_is_number(char *str);
+// TIME.C
+
+long long	get_time(void);
+
+long long	time_diff(long long time);
 
 #endif
