851dd2d89583628fef215663bdbd9e7fe047310e
CanIBeFii <filipe250502@gmail.com>
2023-05-10 16:40:15 +0100

    feat: program now handles 1 or more philosophers

--- sources/philosophers.c
+++ sources/philosophers.c
@@ -6,7 +6,7 @@
 /*   By: fialexan <fialexan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 17:30:33 by fialexan          #+#    #+#             */
-/*   Updated: 2023/05/09 15:48:10 by fialexan         ###   ########.fr       */
+/*   Updated: 2023/05/10 16:34:31 by fialexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,32 +21,41 @@ int	main(int argc, char **argv)
 	{
 		return (1);
 	}
-	philos = init_philos(&info);
+	philos = init_philos(info);
 	if (philos == NULL)
 	{
 		free_info(&info);
 		return (1);
 	}
-	start_dinner(&info, philos);
+	start_dinner(info, philos);
 	free_info(&info);
 	free(philos);
 	return (0);
 }
 
-void	start_dinner(t_info *info, t_philo *philos)
+void	start_dinner(t_info info, t_philo *philos)
 {
-	int	iter;
+	int			iter;
+	long long	time;
 
-	iter = 0;
-	while (iter < info->total_philos)
+	iter = -1;
+	if (info.total_philos == 1)
 	{
-		pthread_create(&philos[iter].thread, NULL, &dinner, &philos[iter]);
-		iter++;
+		philos[0].info = &info;
+		pthread_create(&philos[0].thread, NULL, &one_philo, &philos[0]);
 	}
-	iter = 0;
-	while (iter < info->total_philos)
+	else
 	{
+		time = get_time();
+		while (++iter < info.total_philos)
+		{
+			philos[iter].start_time = time;
+			philos[iter].last_meal = time;
+			philos[iter].info = &info;
+			pthread_create(&philos[iter].thread, NULL, &dinner, &philos[iter]);
+		}
+	}
+	iter = -1;
+	while (++iter < info.total_philos)
 		pthread_join(philos[iter].thread, NULL);
-		iter++;
-	}
 }
