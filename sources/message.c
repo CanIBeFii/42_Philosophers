af288a09d2f30e99706f76e2d9fc1caaf6bd0a97
CanIBeFii <filipe250502@gmail.com>
2023-05-18 17:29:30 +0100

    fix: message printing is more streamlined

--- sources/message.c
+++ sources/message.c
@@ -3,36 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   message.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fialexan <fialexan@student.42.fr>          +#+  +:+       +#+        */
+/*   By: canibefii <canibefii@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/11 12:41:40 by fialexan          #+#    #+#             */
-/*   Updated: 2023/05/11 15:50:55 by fialexan         ###   ########.fr       */
+/*   Created: 2023/05/18 16:25:24 by canibefii         #+#    #+#             */
+/*   Updated: 2023/05/18 16:33:43 by canibefii        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philosophers.h"
 
-void	print_message(t_philo *philo, int message_code)
+void	print_message(t_philo *philo, char *str)
 {
 	pthread_mutex_lock(philo->info->message);
-	if (message_code == SLEEP_CODE)
-		printf("[%lld] %d is sleeping\n", time_diff(philo->start_time),
-			philo->id);
-	else if (message_code == EAT_CODE)
-		printf("[%lld] %d is eating\n", time_diff(philo->start_time),
-			philo->id);
-	else if (message_code == THINK_CODE)
-		printf("[%lld] %d is thinking\n", time_diff(philo->start_time),
-			philo->id);
-	else if (message_code == DEATH_CODE)
-		printf("[%lld] %d died\n", time_diff(philo->start_time),
-			philo->id);
-	else if (message_code == FORK_CODE)
+	if (ft_strcmp(str, FORK_TAKEN) == 0)
 	{
-		printf("[%lld] %d has taken a fork\n", time_diff(philo->start_time),
-			philo->id);
-		printf("[%lld] %d has taken a fork\n", time_diff(philo->start_time),
-			philo->id);
+		printf("%lld %d %s\n", time_diff(philo->start_time), philo->id, str);
+		printf("%lld %d %s\n", time_diff(philo->start_time), philo->id, str);
 	}
+	else 
+		printf("%lld %d %s\n", time_diff(philo->start_time), philo->id, str);
 	pthread_mutex_unlock(philo->info->message);
 }
